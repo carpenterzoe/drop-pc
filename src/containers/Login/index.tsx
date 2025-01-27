d8e41a19d1436e317819dd9eecafc20f10b1029b
carpenterzoe <carpenterzoe@outlook.com>
2025-01-27 10:20:22 +0800

    feat: antd procomponents 登录表单

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,121 +1,86 @@
 import {
-  AlipayCircleOutlined,
   LockOutlined,
   MobileOutlined,
-  TaobaoCircleOutlined,
-  // UserOutlined,
-  WeiboCircleOutlined,
 } from '@ant-design/icons';
 import {
-  LoginForm,
-  ProConfigProvider,
+  LoginFormPage,
   ProFormCaptcha,
   ProFormCheckbox,
   ProFormText,
-  setAlpha,
 } from '@ant-design/pro-components';
-import {
-  Space, message, theme,
-} from 'antd';
-import type { CSSProperties } from 'react';
-import { useState } from 'react';
-
-type LoginType = 'phone' | 'account';
+import styles from './index.module.less';
 
 export default () => {
-  const { token } = theme.useToken();
-  const [loginType] = useState<LoginType>('phone');
-
-  const iconStyles: CSSProperties = {
-    marginInlineStart: '16px',
-    color: setAlpha(token.colorTextBase, 0.2),
-    fontSize: '24px',
-    verticalAlign: 'middle',
-    cursor: 'pointer',
+  const loginHandler = async () => {
   };
 
   return (
-    <ProConfigProvider hashed={false}>
-      <div style={{ backgroundColor: token.colorBgContainer }}>
-        <LoginForm
-          logo="https://github.githubassets.com/favicons/favicon.png"
-          title="Github"
-          subTitle="全球最大的代码托管平台"
-          actions={(
-            <Space>
-              其他登录方式
-              <AlipayCircleOutlined style={iconStyles} />
-              <TaobaoCircleOutlined style={iconStyles} />
-              <WeiboCircleOutlined style={iconStyles} />
-            </Space>
-          )}
+    <div
+      style={{
+        backgroundColor: 'white',
+        height: '100vh',
+      }}
+      className={styles.container}
+    >
+      <LoginFormPage
+        initialValues={{ tel: '19357227510' }}
+        onFinish={loginHandler}
+        backgroundImageUrl="https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png"
+        logo="http://water-drop-assets.oss-cn-hangzhou.aliyuncs.com/images/henglogo.png"
+      >
+        <ProFormText
+          fieldProps={{
+            size: 'large',
+            prefix: <MobileOutlined className="prefixIcon" />,
+          }}
+          name="mobile"
+          placeholder="手机号"
+          rules={[
+            {
+              required: true,
+              message: '请输入手机号！',
+            },
+            {
+              pattern: /^1\d{10}$/,
+              message: '手机号格式错误！',
+            },
+          ]}
+        />
+        <ProFormCaptcha
+          fieldProps={{
+            size: 'large',
+            prefix: <LockOutlined className="prefixIcon" />,
+          }}
+          captchaProps={{
+            size: 'large',
+          }}
+          placeholder="请输入验证码"
+          captchaTextRender={(timing, count) => {
+            if (timing) {
+              return `${count} ${'获取验证码'}`;
+            }
+            return '获取验证码';
+          }}
+          name="captcha"
+          rules={[
+            {
+              required: true,
+              message: '请输入验证码！',
+            },
+          ]}
+          onGetCaptcha={async () => {
+          }}
+        />
+        <div
+          style={{
+            marginBlockEnd: 24,
+          }}
         >
-          {loginType === 'phone' && (
-            <>
-              <ProFormText
-                fieldProps={{
-                  size: 'large',
-                  prefix: <MobileOutlined className="prefixIcon" />,
-                }}
-                name="mobile"
-                placeholder="手机号"
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入手机号！',
-                  },
-                  {
-                    pattern: /^1\d{10}$/,
-                    message: '手机号格式错误！',
-                  },
-                ]}
-              />
-              <ProFormCaptcha
-                fieldProps={{
-                  size: 'large',
-                  prefix: <LockOutlined className="prefixIcon" />,
-                }}
-                captchaProps={{
-                  size: 'large',
-                }}
-                placeholder="请输入验证码"
-                captchaTextRender={(timing, count) => {
-                  if (timing) {
-                    return `${count} ${'获取验证码'}`;
-                  }
-                  return '获取验证码';
-                }}
-                name="captcha"
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入验证码！',
-                  },
-                ]}
-                onGetCaptcha={async () => {
-                  message.success('获取验证码成功！验证码为：1234');
-                }}
-              />
-            </>
-          )}
-          <div
-            style={{
-              marginBlockEnd: 24,
-            }}
-          >
-            <ProFormCheckbox noStyle name="autoLogin">
-              自动登录
-            </ProFormCheckbox>
-            {/* <a
-              style={{
-                float: 'right',
-              }}
-            >
-              忘记密码
-            </a> */}
-          </div>
-        </LoginForm>
-      </div>
-    </ProConfigProvider>
+          <ProFormCheckbox noStyle name="autoLogin">
+            自动登录
+          </ProFormCheckbox>
+        </div>
+      </LoginFormPage>
+    </div>
   );
 };
