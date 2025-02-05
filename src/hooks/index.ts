805369ca49f13751288bedab6c432e35be8dced5
carpenterzoe <carpenterzoe@outlook.com>
2025-02-05 14:46:37 +0800

    fix: eslint error

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -27,16 +27,16 @@ export const useGoTo = () => {
   ) => {
     // 无key 跳转首页
     if (!pageKey) {
-      nav('/')
-      return
+      nav('/'); // eslint忽然不能标红&自动fix，把插件卸载重装，再重启vscode 解决。
+      return;
     }
 
-    const route = getRouteByKey(pageKey)
+    const route = getRouteByKey(pageKey);
     if (route && route.path) {
       // 纯跳转，无参数
       if (!params) {
-        nav(route.path)
-        return
+        nav(route.path);
+        return;
       }
 
       // /page/:id params: { id: 1 } => /page/1
@@ -47,24 +47,24 @@ export const useGoTo = () => {
       nav(`/${url}`);
     }
   };
-  return { back, go }
+  return { back, go };
 };
 
 // 通过页面的url 匹配查找，返回我们自己定义的路由配置信息
 export const useMatchRoute = () => {
   // 拿到当前路由，react 提供
-  const r = useLocation();  
+  const r = useLocation();
 
   /**
    * 到路由配置中查找匹配
-   * 
+   *
    * useMemo 不让该函数执行次数过多？
-   * ? 所以这里引发一个疑问，useMemo 究竟适合于什么样的场景能做到优化？ 
+   * ? 所以这里引发一个疑问，useMemo 究竟适合于什么样的场景能做到优化？
    * ? 怎么才能判断确实有优化， 这个钩子有没有常见的滥用 引发的副作用
    */
   const route = useMemo(() => {
-    routes.find((item) => matchPath(item.path, r.pathname)) 
-  }, [r.pathname])
+    routes.find((item) => matchPath(item.path, r.pathname));
+  }, [r.pathname]);
 
-  return route
-}
+  return route;
+};
