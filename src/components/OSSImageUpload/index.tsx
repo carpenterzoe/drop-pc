f22da61330be2ed2f9e683b5723bb8137b58819a
carpenterzoe <carpenterzoe@outlook.com>
2025-02-10 16:10:41 +0800

    feat: 图片上传 改成支持多个

--- src/components/OSSImageUpload/index.tsx
+++ src/components/OSSImageUpload/index.tsx
@@ -1,9 +1,11 @@
 import type { UploadProps } from 'antd';
-import type { UploadFile } from 'antd/es/upload/interface';
 import ImgCrop from 'antd-img-crop';
-import { Upload } from 'antd';
-import { GET_OSS_INFO } from '@/graphql/oss';
+import {
+  Upload,
+} from 'antd';
+import type { UploadFile } from 'antd/es/upload/interface';
 import { useQuery } from '@apollo/client';
+import { GET_OSS_INFO } from '@/graphql/oss';
 
 interface OSSDataType {
   dir: string;
@@ -16,32 +18,38 @@ interface OSSDataType {
 
 // value & onChange 是为了让外层的 FormItem 监听到
 interface OSSUploadProps {
-  value?: UploadFile;
-  onChange?: (file?: UploadFile) => void;
+  value?: UploadFile[];
+  label?: string;
+  maxCount?: number;
+  imgCropAspect?: number;
+  onChange?: (files: UploadFile[]) => void;
 }
 
-const OSSImageUpload = ({ value, onChange }: OSSUploadProps) => {
+const OSSImageUpload = ({
+  label,
+  maxCount,
+  imgCropAspect,
+  value,
+  onChange,
+}: OSSUploadProps) => {
   const { data, refetch } = useQuery<{ getOSSInfo: OSSDataType }>(GET_OSS_INFO);
   const OSSData = data?.getOSSInfo;
 
   // 指定上传到哪个服务器地址的哪个桶里
   const getKey = (file: UploadFile) => {
     const suffix = file.name.slice(file.name.lastIndexOf('.'));
+    // uid是上传组件自动生成的唯一值
     const key = `${OSSData?.dir}${file.uid}${suffix}`;
     const url = `${OSSData?.host}/${key}`;
     return { key, url };
   };
 
-  const handleChange: UploadProps['onChange'] = ({ file }) => {
-    if (file.status === 'removed') {
-      onChange?.();
-      return;
-    }
-    const newFile = {
-      ...file,
-      url: file.url || getKey(file).url,
-    };
-    onChange?.(newFile);
+  const handleChange: UploadProps['onChange'] = ({ fileList }) => {
+    const files = fileList.map((f) => ({
+      ...f,
+      url: f.url || getKey(f).url,
+    }));
+    onChange?.(files);
   };
 
   // 上传所需额外参数 或返回上传额外参数的方法
@@ -64,25 +72,27 @@ const OSSImageUpload = ({ value, onChange }: OSSUploadProps) => {
   };
 
   return (
-    <ImgCrop rotate>
+    <ImgCrop rotate aspect={imgCropAspect}>
       <Upload
         name="file"
+        maxCount={maxCount}
         listType="picture-card"
-        fileList={value ? [value] : []}
+        fileList={value}
         action={OSSData?.host}
         onChange={handleChange}
         data={getExtraData}
         beforeUpload={beforeUpload}
       >
-        上传
+        {label}
       </Upload>
     </ImgCrop>
   );
 };
-
 OSSImageUpload.defaultProps = {
+  label: '上传图片',
   value: null,
   onChange: () => {},
+  maxCount: 1,
+  imgCropAspect: 1 / 1,
 };
-
 export default OSSImageUpload;
