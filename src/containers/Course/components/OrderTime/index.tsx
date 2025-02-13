3092e8b13d0a540108850d299b7e822c9f005d09
carpenterzoe <carpenterzoe@outlook.com>
2025-02-13 17:44:21 +0800

    feat: 可约时间 - 数据处理拆分到单独的hook

--- src/containers/Course/components/OrderTime/index.tsx
+++ src/containers/Course/components/OrderTime/index.tsx
@@ -2,22 +2,18 @@ import {
   Drawer, Space, Button, Tabs,
   Col,
   Row,
-  message,
 } from 'antd';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { EditableProTable } from '@ant-design/pro-components';
 import { RedoOutlined, ChromeOutlined } from '@ant-design/icons';
-import {
-  useCourseInfo,
-  useEditCourseInfo,
-} from '@/services/course';
+
 import { omit } from 'lodash';
 import {
   DAYS, getColumns, IDay, isWorkDay,
-  workDays,
 } from './constants';
 
 import style from './index.module.less';
+import { useOrderTime } from './hooks';
 
 interface IProps {
   id: string;
@@ -32,82 +28,18 @@ const OrderTime = ({
 }: IProps) => {
   const [currentDay, setCurrentDay] = useState<IDay>(DAYS[0]);
 
-  // 初始化数据重新放回useQuery处理，因为useLazyQuery在无法重新手动触发请求
-  const { data, refetch, loading } = useCourseInfo(id);
-  const [edit, editLoading] = useEditCourseInfo();
-
-  /**
-   * 这里的逻辑其实是比较清晰的。预约时间的计算时机：
-   * 1. 数据初始化时 2. 切换tab时
-   * 也就是 useMemo 的2个dependencies
-   */
-
-  // useMemo返回的值也是响应式的
-  // ? 为什么这里用useMemo，如果不用这个，还有什么可以实现
-  const orderTime = useMemo(
-    // as IOrderTime[] 显式指定这里肯定有值，否则后面数组操作的时候类型提示可能是undefined，无法数组展开
-    () => data?.reducibleTime.find((item) => item.week
-    === currentDay.key)?.orderTime as IOrderTime[] || [],
-    [data, currentDay],
-  );
-
-  const onSaveHandler = (ot: IOrderTime[]) => {
-    const rt = [...(data?.reducibleTime || [])];
-
-    // orderTime 这里拿到的是一周当中某一天的数据
-    const index = rt.findIndex((item) => item.week === currentDay.key);
-
-    const todayOrderTime = {
-      week: currentDay.key,
-      orderTime: ot,
-    };
-    if (index > -1) {
-      rt[index] = todayOrderTime;
-    } else {
-      rt.push(todayOrderTime);
-    }
-
-    // 编辑成功，传一个回调 数据刷新
-    edit(id, {
-      reducibleTime: rt,
-    }, () => refetch());
-  };
-
-  const onDeleteHandler = (key: number) => {
-    const newOt = orderTime.filter((item) => item.key !== key);
-    onSaveHandler(newOt);
-  };
+  const {
+    orderTime,
+    loading,
+    onSaveHandler,
+    onDeleteHandler,
+    allWorkDaySyncHandler,
+    allWeekSyncHandler,
+  } = useOrderTime(id, currentDay.key);
 
   const onTabChangeHandler = (key: string) => {
     const current = DAYS.find((item) => item.key === key) as IDay; // as IDay，因为这里肯定有值
     setCurrentDay(current);
-  };
-  const allWorkDaySyncHandler = () => {
-    if (!isWorkDay(currentDay.key)) {
-      message.error('当前非工作日');
-      return;
-    }
-    // 工作日 统一生成
-    const workDayRt: IWeekCourse[] = workDays.map((week) => ({
-      week,
-      orderTime: [...orderTime],
-    }));
-    // 周末 保留原来的
-    const weekendRt = (data?.reducibleTime || []).filter((item) => !isWorkDay(item.week));
-    const rt: IWeekCourse[] = [...workDayRt, ...weekendRt];
-    edit(id, {
-      reducibleTime: rt,
-    }, () => refetch());
-  };
-
-  const allWeekSyncHandler = () => {
-    const rt: IWeekCourse[] = DAYS.map((item) => ({
-      week: item.key,
-      orderTime,
-    }));
-    edit(id, {
-      reducibleTime: rt,
-    }, () => refetch());
   };
 
   return (
@@ -129,7 +61,7 @@ const OrderTime = ({
       <Tabs type="card" items={DAYS} onChange={onTabChangeHandler} />
       {/* <IOrderTime> 指定 value 的类型 */}
       <EditableProTable<IOrderTime>
-        loading={loading || editLoading}
+        loading={loading}
         columns={getColumns(onDeleteHandler)}
         headerTitle={(
           <Space>
